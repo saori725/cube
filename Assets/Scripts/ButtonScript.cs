23ca362b794b2c587aa5ada0183a84f36f4e7030
saori <z36.noda.saori@gmail.com>
2018-11-16 06:53:34 +0900

    First

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        //イベント作成
+        EventTrigger.Entry pressDown = new EventTrigger.Entry();
+        pressDown.eventID = EventTriggerType.PointerDown;       //イベントのタイプ設定
+        pressDown.callback.AddListener((data) => { ShowLog(); });   //メソッド登録
+
+        //イベントトリガーにイベント追加
+        GetComponent<EventTrigger>().triggers.Add(pressDown);
+    }
+
+    //ログを出すだけのメソッド
+    public void ShowLog()
+    {
+        Debug.Log("ログ！");
+    }
+
+}
